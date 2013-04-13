a4a9a20c7fb40b51a33156562a9f81477452d2cb
Giorgio Borelli <casaborelli@gmail.com>
2013-04-13 15:00:31 +0200

    pep8

--- setup.py
+++ setup.py
@@ -14,34 +14,35 @@ long_description = (
     + '\n')
 
 
-setup(name='collective.z3cform.colorpicker',
-      version=version,
-      description="Colorpicker widget for Plone",
-      long_description=long_description,
-      classifiers=[
+setup(
+    name='collective.z3cform.colorpicker',
+    version=version,
+    description="Colorpicker widget for Plone",
+    long_description=long_description,
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='plone z3cform colorpicker widget',
-      author='Giorgio Borelli',
-      author_email='giorgio@giorgioborelli.it',
-      url='http://www.giorgioborelli.it',
-      license='GPL',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['collective', 'collective.z3cform'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.CMFCore',
-          'plone.app.z3cform',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
+    ],
+    keywords='plone z3cform colorpicker widget',
+    author='Giorgio Borelli',
+    author_email='giorgio@giorgioborelli.it',
+    url='http://www.giorgioborelli.it',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['collective', 'collective.z3cform'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFCore',
+        'plone.app.z3cform',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
 )
