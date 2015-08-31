e295236f7f69fe97f5339bfd4bc027ff9fa7974d
Maurits van Rees <maurits@vanrees.org>
2015-08-31 16:55:04 +0200

    Renamed txt files to rst files.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 version = '1.3.dev0'
 
 long_description = (
-    open('README.txt').read()
+    open('README.rst').read()
     + '\n' +
     'Contributors\n'
     '============\n'
     + '\n' +
-    open('CONTRIBUTORS.txt').read()
+    open('CONTRIBUTORS.rst').read()
     + '\n' +
-    open('CHANGES.txt').read()
+    open('CHANGES.rst').read()
     + '\n')
 
 
